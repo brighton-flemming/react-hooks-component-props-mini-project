4e9644ec8d5f8182bc9822e7862314dbf8666acf
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-20 01:14:21 +0300

    Mission Passed

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,21 @@ import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
 import ArticleList from "./ArticleList";
+import Article from "./Article";
+import path from "path";
+import logo from "../assets/logo";
 
 console.log(blogData);
 
 function App() {
+  const blogName = "Kermit The Blogg";
+  const blogAbout = "Welcome to my blog. Hope you will find the articles as scrumptious as your morning breakfast was."
   return (
     <div className="App">
-     <Header />
-     <About />
-     <ArticleList />
+     <Header name={blogName}/>
+     <About image={logo} about={blogAbout}/>
+     <ArticleList posts={blogData} />
+     <Article />
 
     </div>
   );
