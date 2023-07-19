f392ab6f89961cf572f93fdf014b829086fb44ac
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-20 01:00:05 +0300

    Mission Passed

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import blogData from "../data/blog";
 import Header from "./Header";
 import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
@@ -10,7 +11,7 @@ function App() {
     <div className="App">
      <Header />
      <About />
-
+     <ArticleList />
 
     </div>
   );
